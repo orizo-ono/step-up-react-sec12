ea4692917c2aedadfb9175f5d8957f5565218521
Hono <>
2023-01-12 23:07:54 +0900

    ### 72.【完】 - ヘッダー実装

---
+++ src/components/templete/HeaderLayout.tsx
@@ -0,0 +1,16 @@
+import { FC, memo, ReactNode } from "react";
+import { Header } from "../organisms/layout/Header";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const HeaderLayout: FC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+});
