0c42e2c680e3c2b9a676c977169114a1eeca1cb4
Hono <>
2023-01-17 23:33:56 +0900

    ### 76. 11:43まで - useStateの初期値を設定し忘れ、1時間吸われた - ログイン機能実装

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,8 +1,14 @@
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import { memo, FC } from "react";
+import { memo, FC, useState, ChangeEvent } from "react";
+import { useAuth } from "../../hooks/useAuth";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: FC = memo(() => {
+  const { login, loading } = useAuth();
+  const [userId, setUserId] = useState("");
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
+  const onclickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -11,8 +17,8 @@ export const Login: FC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="ユーザーID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input placeholder="ユーザーID" onChange={onChangeUserId} value={userId} />
+          <PrimaryButton onClick={onclickLogin}>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
