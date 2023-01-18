db49bde7911e05890262b6b073d40124474305b5
Hono <>
2023-01-18 10:04:03 +0900

    追加

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -7,17 +7,6 @@ import { User } from "../types/api/user";
 export const useAuth = () => {
   const navigate = useNavigate();
   const [ loading, setLoading ] = useState( false );
-  // const login = ( id: string ) => {
-  //   setLoading( true );
-  //   axios.get<User>( `https://jsonplaceholder.typicode.com/users/${ id }` ).then( ( res ) => {
-  //     if ( res.data ) {
-  //       navigate( "/home" );
-  //     } else {
-  //       alert( 'ユーザーが見つかりませんでした' );
-  //     }
-  //   } ).catch( () => alert( 'ログインできません' ) ).finally( () => setLoading( false ) );
-  // };
-
   const login = ( id: string ) => {
     axios.get<User>( `https://jsonplaceholder.typicode.com/users/${ id }` ).then( ( res ) => {
       if ( res.data ) {
